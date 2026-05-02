ac604af42e4a7188d749ebb77e7403acb0df7223
Ect07 <ect07aha@hotmail.com>
2026-05-03 00:21:36 +0800

    build: add tauri plugin 'cli'

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,6 +11,7 @@ use tauri::{
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_cli::init())
         .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let window = app.get_webview_window("main").expect("no main window");
